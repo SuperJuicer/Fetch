4d5edc48adfeb0b7ce3a5766aab7e11a318dbbe3
SuperJuice <wisconsinsara@gmail.com>
2020-09-16 20:46:52 -0700

    App now has state, componentDidMount, isFetching loading statement. It also has ItemList and Item, which may be deleted later.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import './App.css';
 
-const items = [
-  {id: "100", listId: "2", name: "Soap"},
-  {id: "200", listId: "4", name: null},
-  {id: "500", listId: "1", name: "Toilet paper"}
-];
+class App extends React.Component {
+  state = {
+    items: [
+      {id: '200', listId: '2', name: 'Second item'},
+      {id: '100', listId: '4', name: null},
+      {id: '300', listId: '1', name: 'First item'}
+    ],
+    isFetching: false
+  };
 
-function App() {
+  public componentDidMount() {
+    if (this.state.items.length === 0) return;
+  }
 
-  return (
-    <div>
-      <h1>Items grouped by listId and alphabetized by name</h1>
-      <ol>
-        {items.map(item => <li key={item.id}>Group {item.listId}: {item.name}</li>)}
-      </ol>
-    </div>
-  );
+  public render () {
+  
+    return (
+      <div>
+        <h1>Items grouped by listId and alphabetized by name</h1>
+        {this.state.isFetching ? 
+          <p>Loading the list :-)</p> : // If the API call hasn't completed yet
+          <ol>
+            {this.state.items.map(item => <li key={item.id}> Group {item.listId}: {item.name}</li>)}
+          </ol>}    
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Item.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+
+interface IItem {
+    id: string,
+    listId: string,
+    name: string
+}
+
+
+export default IItem;
