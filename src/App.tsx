855fe9b36fd30a81fd215fb4f8d1f6bfbe06ed38
SuperJuice <wisconsinsara@gmail.com>
2020-09-17 21:56:10 -0700

    Got past CORS error.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,50 @@
 import React from 'react';
 import './App.css';
 
+interface IItem {
+  id: string,
+  listId: string,
+  name: string
+}
+
+function getItems(): Promise<void | IItem[]> {
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = 'https://fetch-hiring.s3.amazonaws.com/hiring.json';
+
+  return fetch(proxyurl + url)
+    .then(response => response.json())
+    .then(response => {return response as IItem[]})
+    .catch(() => console.log(`Can't access ${url} response. Blocked by CORS?`));
+}
+
+// async function getItems(this: any) {
+//   const response = await fetch('https://fetch-hiring.s3.amazonaws.com/hiring.json');
+//   console.log(response); // nothing logs
+//   const data = response.json();
+//   this.setState({items: data});
+// }
+
 class App extends React.Component {
   state = {
-    items: [
-      {id: '200', listId: '2', name: 'Second item'},
-      {id: '100', listId: '4', name: null},
-      {id: '300', listId: '1', name: 'First item'}
-    ],
-    isFetching: false
+    items: [],
+    isFetching: true
   };
 
   public componentDidMount() {
-    if (this.state.items.length === 0) return;
+    if (this.state.items.length === 0) getItems();
   }
 
   public render () {
+    const {items, isFetching} = this.state;
   
     return (
       <div>
         <h1>Items grouped by listId and alphabetized by name</h1>
-        {this.state.isFetching ? 
-          <p>Loading the list :-)</p> : // If the API call hasn't completed yet
+        {isFetching ? 
+          <p>Loading the list?</p> : // If the API call hasn't completed yet
           <ol>
-            {this.state.items.map(item => <li key={item.id}> Group {item.listId}: {item.name}</li>)}
+            {items.map((item: { id: string; listId: string; name: string; }) => 
+            <li key={item.id}> Group {item.listId}: {item.name}</li>)}
           </ol>}    
       </div>
     );
