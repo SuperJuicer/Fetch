4cd347fc3fb127793cd83c9a386c3af39d7e81e8
SuperJuice <wisconsinsara@gmail.com>
2020-09-18 13:55:26 -0700

    Loaded and displayed list from remote! Removed unused files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
 import React from 'react';
 import './App.css';
 
-interface IItem {
-  id: string,
-  listId: string,
-  name: string
-}
-
-function getItems(): Promise<void | IItem[]> {
-  const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  const url = 'https://fetch-hiring.s3.amazonaws.com/hiring.json';
-
-  return fetch(proxyurl + url)
-    .then(response => response.json())
-    .then(response => {return response as IItem[]})
-    .catch(() => console.log(`Can't access ${url} response. Blocked by CORS?`));
-}
-
-// async function getItems(this: any) {
-//   const response = await fetch('https://fetch-hiring.s3.amazonaws.com/hiring.json');
-//   console.log(response); // nothing logs
-//   const data = response.json();
-//   this.setState({items: data});
-// }
-
 class App extends React.Component {
   state = {
     items: [],
     isFetching: true
   };
 
-  public componentDidMount() {
-    if (this.state.items.length === 0) getItems();
+  async componentDidMount() {
+    const getItems = () => {
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      const url = 'https://fetch-hiring.s3.amazonaws.com/hiring.json';
+    
+      return fetch(proxyurl + url)
+        .then(response => response.json())
+        .then(response => this.setState({items: response}))   
+        .catch(() => console.log(`Can't access ${url} response. Blocked by CORS?`));
+    }
+
+    if (this.state.items.length === 0) await getItems();
+
+    if (this.state.items.length !== 0) this.setState({isFetching: false});
   }
 
   public render () {
@@ -43,7 +32,7 @@ class App extends React.Component {
         {isFetching ? 
           <p>Loading the list?</p> : // If the API call hasn't completed yet
           <ol>
-            {items.map((item: { id: string; listId: string; name: string; }) => 
+            {items.map((item: { id: number; listId: number; name: string; }) => 
             <li key={item.id}> Group {item.listId}: {item.name}</li>)}
           </ol>}    
       </div>
