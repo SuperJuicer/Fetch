01a145fcea46375c197e465a32709e12859ec1a1
SuperJuice <wisconsinsara@gmail.com>
2020-09-19 00:09:02 -0700

    List sorted by listId and by name

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,58 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  state = {
-    items: [],
-    isFetching: true
-  };
+  state = {items: [], isFetching: true};
 
   async componentDidMount() {
+    
     const getItems = () => {
+      // Need proxyurl to avoid CORS blocking response
       const proxyurl = "https://cors-anywhere.herokuapp.com/";
       const url = 'https://fetch-hiring.s3.amazonaws.com/hiring.json';
     
-      return fetch(proxyurl + url)
+      return fetch(`${proxyurl}${url}`)
         .then(response => response.json())
-        .then(response => this.setState({items: response}))   
-        .catch(() => console.log(`Can't access ${url} response. Blocked by CORS?`));
+        .then (response => {return response})
+        .catch(() => console.log('Something went wrong :-('));
     }
 
-    if (this.state.items.length === 0) await getItems();
+    if (this.state.items.length === 0) {
+      let responseItems = await getItems() as { id: number, listId: number, name: string }[];
+
+      // Perform operations on our array of items
+      if (responseItems.length !== 0) {
+
+        // Remove nulls
+        responseItems = responseItems.filter(x => x['name']);
+
+        // Remove string chars from name
+        for (let i = 0; i < responseItems.length; ++i) {
+          responseItems[i].name = responseItems[i].name.substring(5);
+        }
+
+        // Allow name property to accept number type
+        let itemsWithNameAsNumber = responseItems as unknown as { id: number, listId: number, name: number }[];
+
+        // Cast name from string to number so we can sort
+        for (let i = 0; i < itemsWithNameAsNumber.length; ++i) {
+          itemsWithNameAsNumber[i].name = Number(itemsWithNameAsNumber[i].name);
+        }
+
+        // Sort by listId and by name
+        itemsWithNameAsNumber.sort((x, y) => {
+          if (x.listId < y.listId) return -1;
+          if (y.listId < x.listId) return 1;
+
+          if (x.name < y.name) return -1;
+          if (y.name < x.name) return 1;
+
+          return 0;
+        });
+
+        // Finally, we can set our state!
+        this.setState({items: itemsWithNameAsNumber});
+      }
+    }
 
     if (this.state.items.length !== 0) this.setState({isFetching: false});
   }
@@ -30,10 +65,10 @@ class App extends React.Component {
       <div>
         <h1>Items grouped by listId and alphabetized by name</h1>
         {isFetching ? 
-          <p>Loading the list?</p> : // If the API call hasn't completed yet
+          <p>Loading the list?</p> : // If the API call hasn't completed yet, or if fetching our items doesn't work.
           <ol>
             {items.map((item: { id: number; listId: number; name: string; }) => 
-            <li key={item.id}> Group {item.listId}: {item.name}</li>)}
+            <li key={item.id}> List ID {item.listId}: Item {item.name}</li>)}
           </ol>}    
       </div>
     );
